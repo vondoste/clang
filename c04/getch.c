826e53dabff6f83fccad7d456af0ce7ae1f399be
Steven J. von Dohlen <vondoste@live.unc.edu>
2024-09-03 18:27:52 +0000

    Code from section 4.4 to 4.9

---
+++ c04/getch.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define BUFSIZE 100
+
+static char buf[BUFSIZE];
+static int bufp = 0;
+
+/* getch: get a (possibly pushed back) character */
+int getch(void) {
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+/* ungetch: push a character back on input */
+void ungetch(int c) {
+    if (bufp >= BUFSIZE)
+        printf("ungetch: too many characters\n");
+    else
+        buf[bufp++] = c;
+}
